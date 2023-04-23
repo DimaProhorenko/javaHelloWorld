b6d53533f01db0d7d905fd988e5c47c2271b8b54
DimaProhorenko <dima.study.web@gmail.com>
2023-04-23 11:56:35 +0300

    ...

--- Example.java
+++ Example.java
@@ -1,5 +1,27 @@
 public class Example {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int bottles = 100;
+        String word = "bottles";
+
+        while (bottles >= 0) {
+            String songStr = String.format("%d %s of beer,", bottles, word);
+            if (bottles == 0) {
+                songStr = "No more bottles of beer";
+            }
+
+            System.out.println(songStr + " on the wall,");
+            System.out.println(songStr);
+            if (bottles > 0) {
+                System.out.println(String.format("Take on down pass, it around"));
+            }
+            bottles -= 1;
+            word = "bottles";
+            if (bottles == 1) {
+                word = "bottle";
+            }
+            System.out.println(songStr);
+            System.out.println("");
+            System.out.println("");
+        }
     }
 }
